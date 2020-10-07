6260c26739b7ca87c1beaf83b507d5bc720a4c09
JP <jp.bonner@bonner.jp>
2020-10-07 16:12:51 -0600

    interface functions for impl

--- elixir/lib/elixir_in_action/key_value_store.ex
+++ elixir/lib/elixir_in_action/key_value_store.ex
@@ -4,6 +4,21 @@ defmodule KeyValueStore do
     %{}
   end
 
+  @spec start :: pid
+  def start do
+    ServerProcess.start(KeyValueStore)
+  end
+
+  @spec put(pid, any, any) :: any
+  def put(pid, key, value) do
+    ServerProcess.call(pid, {:put, key, value})
+  end
+
+  @spec get(pid, any) :: any
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
+  end
+
   @spec handle_call({:get, any} | {:put, any, any}, map) :: {any, map}
   def handle_call({:put, key, value}, state) do
     {:ok, Map.put(state, key, value)}
